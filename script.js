136f97bacdab0d988c291dd04c324b0fe674629b
nathuchoa <nathaliauchoa@hotmail.com>
2023-04-13 12:07:27 +0100

    Descriptografando

--- script.js
+++ script.js
@@ -20,5 +20,18 @@ document.getElementById("cript").addEventListener("click", function(e){
 });
 
 
-console.log("Tô funcionado")
+document.getElementById("descript").addEventListener("click", function(e){
+    e.preventDefault()
+
+    var message = messageEnter.value;
+
+    var messageDescript = message.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    messageError.style.display = "none";
+
+    document.getElementById("return").innerHTML = '<div class="content-return-text"><p class="text">' + messageDescript + '</p><button id="copiar" class="btn btn-simple" onclick="copiar()">Copiar</button></div>';
+
+    console.log(messageDescript);
+});
+
 
