083c19faaccd7e6fa593e88bafe7ebbbb9fee836
nathuchoa <nathaliauchoa@hotmail.com>
2023-04-13 14:35:48 +0100

    Debug cripto (não estava aceitanto a letra "a")

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var messageError = document.getElementById("return_img");
 document.getElementById("cript").addEventListener("click", function (e) {
     e.preventDefault()
     var message = messageEnter.value;
-    var messageCript = message.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+    var messageCript = message.replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat").replace(/a/g, "ai");
     messageError.style.display = "none";
     document.getElementById("return").innerHTML = '<div class="content-return-text"><textarea readonly id="text">' + messageCript + '</textarea><button id="copy" class="btn btn-simple" onclick="copy()">Copiar</button></div>';
     console.log(messageCript);
