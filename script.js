3c8b36269fafb5e59e07671d3d7fd61c4aebf1f7
nathuchoa <nathaliauchoa@hotmail.com>
2023-04-13 13:30:34 +0100

    ADD função copiar

--- script.js
+++ script.js
@@ -3,35 +3,30 @@ var messageOutput = document.getElementById("#content-return");
 var messageError = document.getElementById("return_img");
 
 
-var message = messageEnter.value;
-
-document.getElementById("cript").addEventListener("click", function(e){
+document.getElementById("cript").addEventListener("click", function (e) {
     e.preventDefault()
-
     var message = messageEnter.value;
-
     var messageCript = message.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
-
     messageError.style.display = "none";
-
-    document.getElementById("return").innerHTML = '<div class="content-return-text"><p class="text">' + messageCript + '</p><button id="copiar" class="btn btn-simple" onclick="copiar()">Copiar</button></div>';
-
+    document.getElementById("return").innerHTML = '<div class="content-return-text"><textarea readonly id="text">' + messageCript + '</textarea><button id="copy" class="btn btn-simple" onclick="copy()">Copiar</button></div>';
     console.log(messageCript);
 });
 
-
-document.getElementById("descript").addEventListener("click", function(e){
+document.getElementById('descript').addEventListener('click', function (e) {
     e.preventDefault()
-
     var message = messageEnter.value;
-
     var messageDescript = message.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
-
-    messageError.style.display = "none";
-
-    document.getElementById("return").innerHTML = '<div class="content-return-text"><p class="text">' + messageDescript + '</p><button id="copiar" class="btn btn-simple" onclick="copiar()">Copiar</button></div>';
+    messageError.style.display = 'none';
+    document.getElementById('return').innerHTML = '<div class="content-return-text"><textarea readonly id="text">' + messageDescript + '</textarea><button id="copy" class="btn btn-simple" onclick="copy()">Copiar</button></div>';
 
     console.log(messageDescript);
 });
+
+function copy (){
+    var textCopy = document.getElementById('text')
+    textCopy.select();
+    document.execCommand('copy');
+    alert("Texto copiado!")
+};
 
 
